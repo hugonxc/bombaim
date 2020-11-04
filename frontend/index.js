5937407dde3ac683d92f91bc1fc88570c2846bfb
Hugo Carvalho <hugonvsc@gmail.com>
2020-11-04 02:34:34 -0300

    Add Groove upload

--- frontend/index.js
+++ frontend/index.js
@@ -1,38 +1,10 @@
-
-
-function fileUpload(url, f){
-    const data = new FormData();
-    data.append('file', f)
-    
-    return fetch(url, {
-        mode: 'cors',
-        method: 'POST',
-        body: data
-    })
-}
-
-function grooveUpload(event){
-    var url  = 'http://localhost:5000/groove';
-    var f = event.target.files[0];
-    var song = fileUpload(url, f);
-
-    song.then(response => response.json())
-        .then(function(json){
-            alert("Groove: " + json['name'] + "\n" + 
-                  "Status: " + json['status']);
-            location.reload()
-        })
-}
-
-
-
 // App component
 import React from "react"
 import ReactDOM from "react-dom"
 import Header from "./components/header/Header"
 import { Player } from "./components/player/Player"
 import ChordChart from "./components/chord_chart/ChordChart"
-
+import Groove from "./components/groove/Groove"
 
 let root = document.getElementById("root");
 
@@ -41,7 +13,8 @@ class App extends React.Component {
         return(
             <div>
                 <Header />
-                <Player /> 
+                <Groove />
+                <Player />
                 <ChordChart />
             </div>
         )
